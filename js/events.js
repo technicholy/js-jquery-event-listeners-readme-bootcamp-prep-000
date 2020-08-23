06609bd742d4ff4403633773b4fa09050eb39cdd
Clayton Pierce <bamer78@gmail.com>
2020-08-23 01:46:17 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,23 @@
 //define functions here
-
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+  
+}
+function submitIt(){
+  
+}
 $(document).ready(function(){
 
 // call functions here
+
 
 });
